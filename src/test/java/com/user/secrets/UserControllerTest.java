e456a26fc8e4964c41fddfa1b3038bc7b4fdfd19
ayushchoukse1 <ayush.choukse@sjsu.edu>
2016-11-04 18:57:52 -0700

    refactored test cases.

--- src/test/java/com/user/secrets/UserControllerTest.java
+++ src/test/java/com/user/secrets/UserControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
@@ -32,6 +33,7 @@ import org.springframework.web.context.WebApplicationContext;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import com.user.secrets.domain.Authority;
 import com.user.secrets.domain.Secret;
 import com.user.secrets.domain.User;
@@ -61,99 +63,133 @@ public class UserControllerTest {
 			.build();
 	}
 
-//	@Test
-//	public void getPutDeleteWithoutAuthorization() throws Exception {
-//		// GET, PUT, DELETE without any authorization.
-//		URI uri = new URI("/user");
-//		URI uri1 = new URI("/user" + (0 + (int) (Math.random() * 1000)));
-//		mvc.perform(get(uri))
-//			.andExpect(status().isUnauthorized());
-//		mvc.perform(put(uri1))
-//			.andExpect(status().isUnauthorized());
-//		mvc.perform(delete(uri1))
-//			.andExpect(status().isUnauthorized());
-//	}
-//
-//	@Test
-//	public void createUser() throws Exception {
-//		mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
-//			.content(new Gson().toJson(getUser())))
-//			.andExpect(status().isCreated());
-//	}
-//
-//	@Test
-//	public void createDuplicateUser() throws Exception {
-//		User user = getUser();
-//		mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
-//			.content(new Gson().toJson(user)));
-//		mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
-//			.content(new Gson().toJson(user)))
-//			.andExpect(status().isConflict())
-//			.andExpect(content().string("user already exists: " + user.getUsername()));
-//	}
-//
-//	@Test
-//	public void updateUserwithoutAuthorization() throws Exception {
-//		// update user without access token.
-//		User user = getUser();
-//		MvcResult mockResult = mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
-//			.content(new Gson().toJson(user)))
-//			.andExpect(status().isCreated())
-//			.andReturn();
-//		MockHttpServletResponse response = mockResult.getResponse();
-//		JsonObject jsonObject = (new JsonParser()).parse(response.getContentAsString())
-//			.getAsJsonObject();
-//		user.setFirstName("ayush");
-//		user.setLastName("choukse");
-//		user.setEmail("ayush.choukse@gmail.com");
-//		mvc.perform(put("/user" + jsonObject.get("id")
-//			.getAsString()).contentType(MediaType.APPLICATION_JSON)
-//				.content(new Gson().toJson(user)))
-//			.andExpect(status().isUnauthorized());
-//	}
+	@Test
+	public void getPutDeleteWithoutAuthorization() throws Exception {
+		// GET, PUT, DELETE without any authorization.
+		URI uri = new URI("/user");
+		URI uri1 = new URI("/user" + (0 + (int) (Math.random() * 1000)));
+		mvc.perform(get(uri))
+			.andExpect(status().isUnauthorized());
+		mvc.perform(put(uri1))
+			.andExpect(status().isUnauthorized());
+		mvc.perform(delete(uri1))
+			.andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	public void createUser() throws Exception {
+		mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
+			.content(new Gson().toJson(getUser())))
+			.andExpect(status().isCreated());
+	}
+
+	@Test
+	public void createDuplicateUser() throws Exception {
+		User user = getUser();
+		persistUser(user);
+		mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
+			.content(new Gson().toJson(user)))
+			.andExpect(status().isConflict())
+			.andExpect(content().string("user already exists: " + user.getUsername()));
+	}
+
+	@Test
+	public void updateUserwithoutAuthorization() throws Exception {
+		// update user without access token.
+		User user = getUser();
+		MvcResult mockResult = persistUser(user);
+		JsonObject jsonObject = getJsonFromResult(mockResult);
+
+		// update user fields.
+		user.setFirstName("ayush");
+		user.setLastName("choukse");
+		user.setEmail("ayush.choukse@gmail.com");
+
+		mvc.perform(put("/user" + jsonObject.get("id")
+			.getAsString()).contentType(MediaType.APPLICATION_JSON)
+				.content(new Gson().toJson(user)))
+			.andExpect(status().isUnauthorized());
+	}
 
 	@Test
 	public void updateUserwithAuthorization() throws Exception {
 		// update user with access token.
 		User user = getUser();
-		System.out.println(user.toString());
+
 		// post user.
-		MvcResult postMockResult = mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
-			.content(new Gson().toJson(user)))
-			.andExpect(status().isCreated())
-			.andReturn();
+		MvcResult postMockResult = persistUser(user);
 
 		// extract id from postMockResult
-		MockHttpServletResponse postMockResponse = postMockResult.getResponse();
-		JsonObject jsonObject = (new JsonParser()).parse(postMockResponse.getContentAsString())
-			.getAsJsonObject();
+		JsonObject jsonObject = getJsonFromResult(postMockResult);
 		String id = jsonObject.get("id")
 			.getAsString();
-		System.out.println("userId from postMock"+id);
+
 		// get access token.
 		MvcResult tokenMockResult = mvc.perform(post("/oauth/token").accept(MediaType.APPLICATION_JSON)
 			.contentType(MediaType.APPLICATION_JSON)
 			.content(new Gson().toJson(new JwtAuthenticationRequest(user.getUsername(), user.getPassword()))))
 			.andReturn();
-		
-		//extract token from tokenMockResult.
-		MockHttpServletResponse postTokenResponse = tokenMockResult.getResponse();
-		JsonObject jsonTokenObject = (new JsonParser()).parse(postTokenResponse.getContentAsString())
-			.getAsJsonObject();
+
+		// extract token from tokenMockResult.
+		JsonObject jsonTokenObject = getJsonFromResult(tokenMockResult);
+
 		String accessToken = jsonTokenObject.get("token")
 			.getAsString();
-		System.out.println("token from token mock"+accessToken);
+
 		// update user.
 		user.setFirstName("ayush");
 		user.setLastName("choukse");
 		user.setEmail("ayush.choukse@gmail.com");
-		
+
 		// perform put operation.
 		mvc.perform(put("/user/" + jsonObject.get("id")
 			.getAsString()).contentType(MediaType.APPLICATION_JSON)
 				.header("Authorization", accessToken)
 				.content(new Gson().toJson(user)))
 			.andExpect(status().isOk());
+	}
+
+	@Test
+	public void updateAnotherUserwithAuthorization() throws Exception {
+		// update user with access token.
+
+		User user1 = getUser();
+		User user2 = getUser();
+
+		// persist user2.
+		persistUser(user2);
+
+		// post user1.
+		MvcResult postMockResult = persistUser(user1);
+
+		// extract id from postMockResult
+		JsonObject jsonObject = getJsonFromResult(postMockResult);
+		String id = jsonObject.get("id")
+			.getAsString();
+
+		// get access token.
+		MvcResult tokenMockResult = mvc.perform(post("/oauth/token").accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(new Gson().toJson(new JwtAuthenticationRequest(user1.getUsername(), user1.getPassword()))))
+			.andReturn();
+
+		// extract token from tokenMockResult.
+		JsonObject jsonTokenObject = getJsonFromResult(tokenMockResult);
+		String accessToken = jsonTokenObject.get("token")
+			.getAsString();
+
+		// update user2 fields.
+		user2.setFirstName("ayush");
+		user2.setLastName("choukse");
+		user2.setEmail("ayush.choukse@gmail.com");
+		Long user2Id = userServiceImpl.findByUserName(user2.getUsername())
+			.getId();
+
+		// perform update operation on user2 with access token from user1.
+		mvc.perform(put("/user/" + user2Id).contentType(MediaType.APPLICATION_JSON)
+			.header("Authorization", accessToken)
+			.content(new Gson().toJson(user2)))
+			.andExpect(status().isForbidden());
 	}
 
 	public User getUser() {
@@ -163,5 +199,19 @@ public class UserControllerTest {
 				RandomStringUtils.randomAlphanumeric(5), RandomStringUtils.randomAlphanumeric(5), null, null, null,
 				null);
 
+	}
+
+	public MvcResult persistUser(User user) throws Exception {
+		return mvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON)
+			.content(new Gson().toJson(user)))
+			.andExpect(status().isCreated())
+			.andReturn();
+	}
+
+	public JsonObject getJsonFromResult(MvcResult result) throws JsonSyntaxException, UnsupportedEncodingException {
+		MockHttpServletResponse response = result.getResponse();
+		JsonObject jsonObject = (new JsonParser()).parse(response.getContentAsString())
+			.getAsJsonObject();
+		return jsonObject;
 	}
 }
